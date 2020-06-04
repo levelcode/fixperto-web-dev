7f7fefaa4fe40a480453813daa91d8f768909d86
Alain Arturo Chacon Casas <alainarturoc@gmail.com>
2020-06-04 17:09:22 -0500

    perfil

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import Core from "./Core";
 import Ingreso from "./vistas/ingreso/ingresoVista";
 import Registro from "./vistas/registro/registroVista";
 import Beneficios from "./vistas/registro/beneficiosVista";
+import Perfil from "./vistas/perfil/perfilVista";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 
 const routes = (
@@ -14,7 +15,8 @@ const routes = (
 			<Route path="/ingreso" component={Ingreso} />
 			<Route path="/registro" component={Registro} />
 			<Route path="/beneficios" component={Beneficios} />
-			<Redirect from="/" to="/ingreso" />
+			<Route path="/perfil" component={Perfil} />
+			<Redirect from="/" to="/perfil" />
 		</Switch>
 	</BrowserRouter>
 );
