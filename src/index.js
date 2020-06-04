055ee187884b9e391a6f32fa19506468d78dd60e
Alain Arturo Chacon Casas <alainarturoc@gmail.com>
2020-06-04 15:14:37 -0500

    pagina de inicio

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import 'w3-css/w3.css';
 import Core from "./Core";
 import Ingreso from "./vistas/ingreso/ingresoVista";
-import Login from "./vistas/login/loginVista";
 import Registro from "./vistas/registro/registroVista";
 import Beneficios from "./vistas/registro/beneficiosVista";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
@@ -13,7 +12,6 @@ const routes = (
 		<Switch>
 			<Route path="/home" component={Core} />
 			<Route path="/ingreso" component={Ingreso} />
-			<Route path="/login" component={Login} />
 			<Route path="/registro" component={Registro} />
 			<Route path="/beneficios" component={Beneficios} />
 			<Redirect from="/" to="/ingreso" />
