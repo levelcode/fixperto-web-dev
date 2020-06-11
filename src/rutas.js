b326bb86c890de00caf8241351c7e8375a7840b3
Ambar Ballen <ambarballen123@gmail.com>
2020-06-11 17:10:51 -0500

    Cambios en rutas

--- src/rutas.js
+++ src/rutas.js
@@ -2,12 +2,14 @@ import Ingreso from "./vistas/ingreso/ingresoVista";
 import App from './App';
 import Beneficios from "./vistas/registro/beneficiosVista";
 import Registro from "./vistas/registro/registroVista";
+import Perfil from "./vistas/perfil/perfilVista";
 const rutas = [
 	{ path: "/ingreso", text: 'Ingreso', component: Ingreso },
 	{
 		path: "/fixperto", text: 'App', component: App, sub_routes: [
 			{ path: "/beneficios", text: 'Beneficios', component: Beneficios },
 			{ path: "/registro", text: 'Registro', component: Registro },
+			{ path: "/perfil", text: 'Perfil', component: Perfil },
 
 		]
 	}
