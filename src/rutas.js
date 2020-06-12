5b664b78eac2fa8d5cce95999afece56549a818c
Ambar Ballen <ambarballen123@gmail.com>
2020-06-12 15:06:54 -0500

    Cambios perfil

--- src/rutas.js
+++ src/rutas.js
@@ -3,6 +3,10 @@ import App from './App';
 import Beneficios from "./vistas/registro/beneficiosVista";
 import Registro from "./vistas/registro/registroVista";
 import Perfil from "./vistas/perfil/perfilVista";
+import PerfilInformacion from "./vistas/perfil/perfilInformacion";
+import PerfilConfig from "./vistas/perfil/perfilConfig";
+import PerfilAtencionCliente from "./vistas/perfil/perfilAtencionCliente";
+import PerfilTerminos from "./vistas/perfil/perfilTerminos";
 const rutas = [
 	{ path: "/ingreso", text: 'Ingreso', component: Ingreso },
 	{
@@ -10,7 +14,10 @@ const rutas = [
 			{ path: "/beneficios", text: 'Beneficios', component: Beneficios },
 			{ path: "/registro", text: 'Registro', component: Registro },
 			{ path: "/perfil", text: 'Perfil', component: Perfil },
-
+			{ path: "/perfil-informacion", text: 'Perfil Informacion', component: PerfilInformacion },
+			{ path: "/perfil-config", text: 'Perfil Configuración', component: PerfilConfig },
+			{ path: "/perfil-atencion", text: 'Perfil Atencion Cliente', component: PerfilAtencionCliente },
+			{ path: "/perfil-terminos", text: 'Perfil Terminos', component: PerfilTerminos },
 		]
 	}
 ];
