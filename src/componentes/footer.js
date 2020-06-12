2ee4635cb453bedb7e9bfdf643766775b3855407
alainshiftactive <alain.chacon@shiftactive.com>
2020-06-12 15:15:12 -0500

    footer

---
+++ src/componentes/footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+const Footer = (props) => {
+	return (
+		<React.Fragment>
+			<div className="w3-bar fondPage w3-border-top w3-border-blue ">
+				<div className="w3-quarter w3-container w3-section w3-center" >
+					<img src="./assets/fixperto.png" className="footerFixperto" alt="Norway" />
+				</div>
+				<div className="w3-half w3-right w3-section w3-text-white" >
+					<b>Descarga fixperto</b>
+					<div>
+						<img src="./assets/google_play.png" className="footerFixperto" alt="Norway" />
+						<img src="./assets/app_store.png" className="footerFixperto" alt="Norway" />
+					</div>
+					<p style={{ fontSize: 10 + "px" }}>2020 fixperto Todos los derechos reservados</p>
+				</div>
+			</div>
+		</React.Fragment >
+	);
+}
+export default Footer;
--- src/vistas/ingreso/ingresoVista.js
+++ src/vistas/ingreso/ingresoVista.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import Login from "../login/loginVista";
 import HeaderInicio from "../../componentes/headerInicio";
+import Footer from "../../componentes/footer";
 class Ingreso extends React.Component {
 	constructor(props) { super(props); this.state = {}; }
 	render() {
 		return (
 			<React.Fragment>
 				<HeaderInicio history={this.props["history"]} />
-				<div className="w3-row">
-					<div className="w3-quarter w3-container" />
-					<div className="w3-half w3-container w3-center">
-						<div className="w3-container fondPage w3-text-white w3-round-large w3-margin-top">
-						<br />
-							<h1><b>Bienvenido</b></h1>
-							<p className="w3-section">Todo lo que necesitas en arreglos para tu <b>casa, oficina y trabajo</b> en un sólo lugar.</p>
-							<br />
-							<br />
-							<Login history={this.props["history"]} />
+				<div className="fondPage">
+					<div className="w3-row w3-margin-bottom">
+						<div className="w3-quarter w3-container" />
+						<div className="w3-half w3-container w3-center">
+							<div className="w3-container fondPage w3-text-white w3-round-large w3-margin-top">
+								<br />
+								<h1><b>Bienvenido</b></h1>
+								<p className="w3-section">Todo lo que necesitas en arreglos para tu <b>casa, oficina y trabajo</b> en un sólo lugar.</p>
+								<br />
+								<br />
+								<Login history={this.props["history"]} />
+							</div>
 						</div>
+						<div className="w3-quarter w3-container" />
 					</div>
-					<div className="w3-quarter w3-container" />
+					<Footer />
 				</div>
 			</React.Fragment >
 		);
