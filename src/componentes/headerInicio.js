709c79c2e56a474c64424d7944a24975f325e735
alainshiftactive <alain.chacon@shiftactive.com>
2020-06-20 13:34:05 -0500

    header inicio

--- src/componentes/headerInicio.js
+++ src/componentes/headerInicio.js
@@ -1,20 +1,26 @@
 import React from "react";
 const HeaderInicio = (props) => {
-	const registrar = () => { props["history"]["push"]("fixperto/registro"); }
 	return (
 		<React.Fragment>
-			<div className="w3-bar w3-row-padding w3-border w3-card w3-padding-16">
-				<div className="w3-quarter w3-container w3-center" >
+			<div className="w3-bar w3-cell-row w3-border w3-card w3-padding-16">
+				<div className="w3-cell w3-container" style={{width:80+"%"}}>
 					<img src="./assets/fixperto1.png" className="headerFixperto" alt="Norway" />
 				</div>
-				<div className="w3-half" ><p /></div>
-				<div className="w3-quarter w3-container w3-center">
-					<b onClick={()=>{}}>Home</b>
-					<div className="w3-button w3-round-xlarge w3-margin-left w3-text-white"
-						style={{ backgroundColor: "#43AECC" }} onClick={()=>{}}>Home</div>
+				<div className="w3-cell w3-container w3-cell-row">
+					<div className="w3-cell" style={{width:25+"px"}}>
+						<b onClick={() => { }}>Home</b>
+					</div>
+					<div className="w3-cell">
+						<p> | </p>
+					</div>
+					<div className="w3-cell" style={{width:25+"px"}}>
+						<b onClick={() => { }}>Blog</b>
+					</div>
 				</div>
 			</div>
 		</React.Fragment >
 	);
 }
 export default HeaderInicio;
+
+
