0451694acab9f0c65e1948efc193f0c7d50f1d35
alainshiftactive <alain.chacon@shiftactive.com>
2020-06-20 11:26:39 -0500

    footer y denominacion de la accion

--- src/vistas/servicios/serviciosVista.js
+++ src/vistas/servicios/serviciosVista.js
@@ -6,98 +6,98 @@ import httpClient from "../../constantes/axios";
 
 class ServiciosVista extends React.Component {
 
-    constructor(props) { 
-        super(props); 
-        this.state = { 
-            textoAlert      : "", 
-            showAlert       : false, 
-            user            : [], 
-            services        : [], 
-            copy            : [], 
-            search          : "", 
-            new_categori    : "",
-            name            : "",
-        }
-    }
-
-    componentDidMount() { 
-        var user = JSON.parse(localStorage.getItem("@USER"))
-        this.setState({user})
-
-        this.state['user'].push(user)
-
-        this.setState({name : this.state['user'][0]['name']});
-
-        this.getServices()
-    }
-
-    getServices = () => {
-        var me = this
-        axios({
-            method: 'post',
-            url: httpClient.urlBase + '/services/getServices',
-            headers: { Accept: 'application/json' }
-        })
-        .then(function (responseJson) {
-
-            responseJson = responseJson['data']
-            
-            me.setState({ 
-                services: responseJson.services, 
-                copy: responseJson.services 
-            })
-
-            
-        })
-        .catch((error) => {
-            if (error.message === 'Timeout' || error.message === 'Network request failed') {
-                me.setState({ showAlert: true, textoAlert: "Problemas de conexión" });
-            }
-        })	
+	constructor(props) {
+		super(props);
+		this.state = {
+			textoAlert: "",
+			showAlert: false,
+			user: [],
+			services: [],
+			copy: [],
+			search: "",
+			new_categori: "",
+			name: "",
+		}
 	}
 
-    updateSearch = search => {
+	componentDidMount() {
+		var user = JSON.parse(localStorage.getItem("@USER"))
+		this.setState({ user })
+
+		this.state['user'].push(user)
+
+		this.setState({ name: this.state['user'][0]['name'] });
+
+		this.getServices()
+	}
+
+	getServices = () => {
+		var me = this
+		axios({
+			method: 'post',
+			url: httpClient.urlBase + '/services/getServices',
+			headers: { Accept: 'application/json' }
+		})
+			.then(function (responseJson) {
+
+				responseJson = responseJson['data']
+
+				me.setState({
+					services: responseJson.services,
+					copy: responseJson.services
+				})
+
+
+			})
+			.catch((error) => {
+				if (error.message === 'Timeout' || error.message === 'Network request failed') {
+					me.setState({ showAlert: true, textoAlert: "Problemas de conexión" });
+				}
+			})
+	}
+
+	updateSearch = search => {
 		let copy = this.state["services"].filter(service => (service["denomination"].toLowerCase()).indexOf(search.toLowerCase()) !== -1);
 		this.setState({ search, copy });
 	};
 
-    continuarCateg(item) { 
+	continuarCateg(item) {
 
-        this.props.history.push({
-            pathname: '/fixperto/servicios-categ',
-            item
-        })
-    }
+		this.props.history.push({
+			pathname: '/fixperto/servicios-categ',
+			item
+		})
+	}
 
-    addCategory = () => {
-        var me = this
-        axios({
-            method: 'post',
-            url: httpClient.urlBase + '/services/addCategoriaSugerida',
-            data: { 
-                denomination: this.state["new_categori"]
-            },
-            headers: { Accept: 'application/json' }
-        })
-        .then(function (responseJson) {
+	addCategory = () => {
+		var me = this
+		axios({
+			method: 'post',
+			url: httpClient.urlBase + '/services/addCategoriaSugerida',
+			data: {
+				denomination: this.state["new_categori"]
+			},
+			headers: { Accept: 'application/json' }
+		})
+			.then(function (responseJson) {
 
-            responseJson = responseJson['data']
+				responseJson = responseJson['data']
 
-            if (responseJson.success) {
-                me.setState({ showAlert: true, textoAlert: "Gracias por tu mensaje", new_categori: "", search: "", copy: this.state["services"] });
-            }else { 
-                me.setState({ showAlert: true, textoAlert: "Ha ocurrido un error, inténtelo nuevamente" });
-            }
-        })
-        .catch((error) => {
-            if (error.message === 'Timeout' || error.message === 'Network request failed') {
-                me.setState({ showAlert: true, textoAlert: "Problemas de conexión" });
-            }
-        })
+				if (responseJson.success) {
+					me.setState({ showAlert: true, textoAlert: "Gracias por tu mensaje", new_categori: "", search: "", copy: this.state["services"] });
+				} else {
+					me.setState({ showAlert: true, textoAlert: "Ha ocurrido un error, inténtelo nuevamente" });
+				}
+			})
+			.catch((error) => {
+				if (error.message === 'Timeout' || error.message === 'Network request failed') {
+					me.setState({ showAlert: true, textoAlert: "Problemas de conexión" });
+				}
+			})
 	}
 
 	render() {
-        const { textoAlert, showAlert, search, new_categori} = this.state;
+		const { textoAlert, showAlert, search, new_categori } = this.state;
 
 		return (
 			<React.Fragment>
@@ -105,27 +105,32 @@ class ServiciosVista extends React.Component {
 				<div className="servicios">
 
 					<div className="buscador">
-                        <h3> ¿<b>{this.state['name']}</b>,</h3>
-                        <h3>necesitas un experto en.. ?</h3>
+						<h3> ¿<b>{this.state['name']}</b>,</h3>
+						<h3>necesitas un experto en.. ?</h3>
 
-                        <div>
-                            <input className="w3-input" type="text" value={search} 
-							onChange={(e) => this.updateSearch(e.target.value)} />
-                        </div>
+						<div>
+							<input className="w3-input" type="text" value={search}
+								onChange={(e) => this.updateSearch(e.target.value)} />
+						</div>
 
 					</div>
 
-                    <div className="list_servicios">
+					<div className="list_servicios">
+						<div className="w3-cell-row w3-margin-bottom">
+							<div className="w3-cell" style={{ width: 35 + "px" }}>
+								<img src="../../assets/solicitudesUp.png" alt="Norway" />
+							</div>
+							<h2 className="w3-cell" >Servicios</h2>
+						</div>
+						{
+							this.state["copy"].length === 0 && this.state["search"] !== "" &&
+							<div className="add_cat">
+								<p>Escribe aquí la categoría que estás buscando : </p>
 
-                        {
-                        this.state["copy"].length === 0 && this.state["search"] !== "" &&
-                            <div className="add_cat">
-                                <p>Escribe aquí la categoría que estás buscando : </p>
+								<input className="w3-round-large" type="text" value={new_categori}
+									onChange={(e) => this.setState({ new_categori: e.target.value })} />
 
-                                <input className="w3-round-large" type="text" value={new_categori} 
-								onChange={(e) => this.setState({ new_categori: e.target.value })} />
-                
-                                <p className="p_btn">
+								<p className="p_btn">
 									<button className="w3-button btn"
 										onClick={(e) => {
 											e.preventDefault();
@@ -133,25 +138,25 @@ class ServiciosVista extends React.Component {
 										}}>Enviar
 									</button>
 								</p>
-                            </div>
-                        }
+							</div>
+						}
 
-                        <div className="w3-row">
-                            {this.state.services.map((item, key )=> (
-                            <div className="w3-col s6 m3 l2" key={key} 
-                                onClick={(e) => {
-									e.preventDefault();
-									this.continuarCateg(item);
-								}}>
-                                <div className="w3-card card_serv">
-                                    <img src={item.icon} className="img_serv" alt=""></img>
-                                    <p>{item.denomination}</p>
-                                </div>
-                            </div>
-                            ))}
-                        </div>
+						<div className="w3-row">
+							{this.state.services.map((item, key) => (
+								<div className="w3-col s6 m3 l2" key={key}
+									onClick={(e) => {
+										e.preventDefault();
+										this.continuarCateg(item);
+									}}>
+									<div className="w3-card card_serv">
+										<img src={item.icon} className="img_serv" alt=""></img>
+										<p>{item.denomination}</p>
+									</div>
+								</div>
+							))}
+						</div>
 
-                    </div>
+					</div>
 				</div>
 			</React.Fragment >
 		);
