deeb3257f0c34f420be8baede0c59afc535ac535
alainshiftactive <alain.chacon@shiftactive.com>
2020-06-22 16:25:54 -0500

    fotos de las solicitud

--- src/componentes/solicitud.js
+++ src/componentes/solicitud.js
@@ -3,6 +3,7 @@ import httpClient from "../constantes/axios";
 import Alerta from "./alertaVista";
 import axios from "axios";
 const Solicitud = (props) => {
+	const [actualizar, setActualizar] = useState(true);
 	const [request, setRequest] = useState({});
 	const [showAlert, setShowAlert] = useState(false);
 	const [textoAlert, setTextoAlert] = useState("");
@@ -12,19 +13,19 @@ const Solicitud = (props) => {
 			data: { id: props["request"] }, headers: { Accept: 'application/json' }
 		})
 			.then(function (responseJson) {
-				if (responseJson["data"]["success"]) { setRequest(responseJson["data"]["request"]); }
+				if (responseJson["data"]["success"]) { setRequest(responseJson["data"]["request"]); setActualizar(false); }
 				else { setTextoAlert("Ha ocurrido un erro, intente nuevamente"); setShowAlert(true); }
 			})
 			.catch(function (response) { setTextoAlert("Problemas de conexión."); setShowAlert(true); });
 	}
-	if (props["show"]) { getRequest() }
+	if (props["show"] && actualizar) { getRequest(); }
 	return (
 		<React.Fragment>
 			<Alerta showAlert={showAlert} textoAlert={textoAlert} close={() => setShowAlert(false)} />
 			<div className="w3-modal w3-text-black" style={{ display: (props["show"]) ? "flex" : "none" }}>
 				<div className="w3-modal-content w3-animate-top w3-round-large" style={{ width: 50 + '%' }}>
 					<div className="w3-container w3-margin-top w3-margin-bottom">
-						<span onClick={() => props["close"]()}
+						<span onClick={() => { setActualizar(true); props["close"](); }}
 							className="w3-button w3-display-topright w3-round-small w3-hover-red">&times;</span>
 						<br />
 						<div className="w3-section w3-card">
@@ -72,13 +73,11 @@ const Solicitud = (props) => {
 								</div>
 							</div>
 						</div>
-						{request["photos"] && request["photos"].length > 0 && <div className="w3-section w3-center">Fotos</div>}
-						<div className="w3-row">
+						{request["photos"] && request["photos"].length > 0 && <div className="w3-section w3-center"><b>Fotos</b></div>}
+						<div className="w3-row-padding">
 							{request["photos"] && request["photos"].length > 0 && request["photos"].map((photo, key) => (
-								<div className="w3-col s6 m3 l2" key={key}>
-									<div className="w3-card card_serv">
-										<img src={"https://api.fixperto.com/uploads/requests/" + photo["img"]} className="img_serv" alt="Foto"></img>
-									</div>
+								<div className="w3-quarter" key={key}>
+									<img src={"https://api.fixperto.com/uploads/requests/" + photo["image"]} className="imagen-experto" alt="Foto"></img>
 								</div>
 							))}
 						</div>
