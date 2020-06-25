185ff823e7e81afd50ba4a27c4c2bf767953ef77
Ambar Ballen <ambarballen123@gmail.com>
2020-06-25 11:00:05 -0500

    Cambios

--- src/vistas/solicitudes/agendado/detalleAgendadoVista.js
+++ src/vistas/solicitudes/agendado/detalleAgendadoVista.js
@@ -79,7 +79,7 @@ class DetalleAgendado extends React.Component {
 				{id === this.props["request"] && <div>
 					<div className="detalle_solic">
 						<div className="w3-cell">
-							<div className="w3-card icon_card_detalle">
+							<div className=" icon_card_detalle">
 								{(request.icon) ?
 									<img src={request.icon} className="imagen-solicitud  " alt="Imagen" />
 									: <img src="../../../../assets/icon.png" className="  " alt="Imagen" />
