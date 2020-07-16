d61b372407844ee8de7bfbad38ef957b97c35c6d
alainshiftactive <alain.chacon@shiftactive.com>
2020-07-16 12:43:10 -0500

    tarea 21 dashboard

--- src/vistas/perfil/perfilInformacion.js
+++ src/vistas/perfil/perfilInformacion.js
@@ -3,232 +3,146 @@ import Alerta from "../../componentes/alertaVista";
 import { validateEmail, validateName, validatePhone, fechaAutorizada } from "../../constantes/funciones_auxiliares";
 import axios from "axios";
 import httpClient from "../../constantes/axios";
-
-
-class Registro extends React.Component {
-
+class PerfilInformacion extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = { 
-			textoAlert			: "", 
-			showAlert			: false, 
-			buttonDisabled		: false, 
-			isModalVisible		: false, 
-			id					: "", 
-			typeId				: "", 
-			name				: "", 
-			email				: "", 
-			birth_date			: "", 
-			gender				: "", 
-			phone				: "",
-			user 				: []
+		this.state = {
+			textoAlert: "", showAlert: false, buttonDisabled: false, isModalVisible: false,
+			id: "", typeId: "", name: "", email: "", birth_date: "", gender: "", phone: ""
 		}
 	}
-
-	componentDidMount() {
-	
-		var user = JSON.parse(localStorage.getItem("@USER"))
-
-        this.state['user'].push(user)
-
-		this.getInformation()
-	}
-
+	componentDidMount() { this.getInformation(); }
+	fechaNacimiento = (fecha) => { var fechaa = fecha.split('/'); return fechaa[2] + "-" + fechaa[1] + "-" + fechaa[0]; }
 	getInformation = () => {
-		var id = this.state['user'][0]['id']
-		var me = this
-        axios({
-            method: 'post',
-            url: httpClient.urlBase + '/cliente/getInformation',
-			data : { id : id },
-            headers: { Accept: 'application/json' },
-        })
-        .then(function (responseJson) {
-            var datos = responseJson['data']['customer']
+		var id = JSON.parse(localStorage.getItem("@USER"))["id"];
+		var me = this;
+		axios({
+			method: 'post',
+			url: httpClient.urlBase + '/cliente/getInformation',
+			data: { id: id },
+			headers: { Accept: 'application/json' },
+		}).then(function (responseJson) {
+			var datos = responseJson['data']['customer'];
 			me.setState({
-				id			: datos["id"],
-				name 		: datos["name"],
-				email		: datos["email"],
-				birth_date	: (datos["birth_date"]) ? me.fechaAutorizada(datos["birth_date"]) : "",
-				gender		: datos["gender"],
-				phone		: datos["phone"].toString(),
-				typeId		: datos["typeId"],
-			})
-
-        })
-        .catch((error) => {
-            if (error.message === 'Timeout' || error.message === 'Network request failed') {
+				id: datos["id"],
+				name: datos["name"],
+				email: datos["email"],
+				birth_date: (datos["birth_date"]) ? me.fechaNacimiento(datos["birth_date"]) : "",
+				gender: datos["gender"],
+				phone: datos["phone"].toString(),
+				typeId: datos["typeId"],
+			});
+		}).catch((error) => {
+			if (error.message === 'Timeout' || error.message === 'Network request failed') {
 				me.setState({ showAlert: true, textoAlert: "Problemas de conexión" });
-			}
-        })
+			} else { me.getInformation(); }
+		})
 	}
-
 	formatDate = date => {
 		let today = new Date(date); return today.getDate() + "/" + parseInt(today.getMonth() + 1) + "/" + today.getFullYear();
 	}
-
-	convertDateTimeUpdate(date) {
-		console.log(date);
-		
-		var from = date.split("/"); return new Date(from[2], from[1] - 1, from[0]); 
-	}
-
-	fechaAutorizada = (fecha) => {
-		var fecha = fecha.split('/');
-		return fecha[2] + "-" + fecha[1] + "-" + fecha[0];
-	}
-
+	convertDateTimeUpdate(date) { var from = date.split("/"); return new Date(from[2], from[1] - 1, from[0]); }
 	convertDateTime = (date) => {
 		var fecha = new Date(date); return fecha.toISOString().split('T')[0] + ' ' + fecha.toTimeString().split(' ')[0];
 	}
-
-	gender_type = [
-		{ 
-			id: 1, 
-			denomination: 'Masculino' 
-		}, 
-		{ 
-			id: 2, 
-			denomination: 'Femenino' 
-		}	
-	];
-	
+	gender_type = [{ id: 1, denomination: 'Masculino' }, { id: 2, denomination: 'Femenino' }];
 	toggleModal = () => { this.setState({ isModalVisible: !this.state["isModalVisible"] }); }
-
-
-	validateEmail = email => { 
-		let reg = /^([A-Za-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/; return reg.test(email.trim()); 
-	};
-
-	validateName = name => { 
-		let reg = /^([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\']+[\s])+([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\'])+[\s]?([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\'])?$/; return reg.test(name); 
-	
-	}
-
-	validatePhone = phone => { 
-		let reg = /^[0-9]{7,10}$/; return reg.test(phone); 
-	}
-
 	guardar = () => {
 		if (this.state["name"] === "" || this.state["email"] === "" || this.state["birth_date"] === ""
 			|| this.state["gender"] === "" || this.state["phone"] === "") {
 			return this.setState({ showAlert: true, textoAlert: "Existen campos vacios" });
 		}
-		else if (!this.validateEmail(this.state["email"])) {
+		else if (!validateEmail(this.state["email"])) {
 			return this.setState({ showAlert: true, textoAlert: "Correo inválido, por favor verifíquelo" });
 		}
-		else if (!this.validatePhone(this.state["phone"])) {
+		else if (!validatePhone(this.state["phone"])) {
 			return this.setState({ showAlert: true, textoAlert: "Teléfono inválido, por favor verifíquelo" });
 		}
-		else if (!this.validateName(this.state["name"])) {
+		else if (!validateName(this.state["name"])) {
 			return this.setState({ showAlert: true, textoAlert: "Nombre y apellido, por favor verifíquelo" });
 		}
 		else {
-			
 			this.setState({ buttonDisabled: true });
-			var me = this
+			var user = JSON.parse(localStorage.getItem("@USER"));
+			var me = this;
 			axios({
 				method: 'post',
 				url: httpClient.urlBase + '/cliente/modInformation',
-				data : { 
-					id			: me.state["id"],
-					typeId		: me.state["typeId"],
-					name		: me.state["name"],
-					email		: me.state["email"],
-					birth_date	: me.convertDateTime(this.state["birth_date"]),
-					gender		: me.state["gender"],
-					phone		: me.state["phone"]
+				data: {
+					id: me.state["id"],
+					typeId: me.state["typeId"],
+					name: me.state["name"],
+					email: me.state["email"],
+					birth_date: me.convertDateTime(this.state["birth_date"]),
+					gender: me.state["gender"],
+					phone: me.state["phone"]
 				},
 				headers: { Accept: 'application/json' },
-			})
-			.then(function (responseJson) {
-				var responseJson = responseJson['data']
+			}).then(function (responseJson) {
+				var responseJson = responseJson['data'];
 				me.setState({ buttonDisabled: false });
-
 				if (responseJson.success) {
 					me.setState({ showAlert: true, textoAlert: "Se ha guardado la informaición" });
-					
-				}else { 
-					if (responseJson.existe) { 
-						this.toggleModal(); 
-					} 
+					user["name"] = me.state["name"];
+					user["email"] = me.state["email"];
+					localStorage.setItem("@USER", JSON.stringify(user));
+					me.props["actualizar"]();
 				}
-
-			})
-			.catch((error) => {
+				else { if (responseJson.existe) { this.toggleModal(); } }
+			}).catch((error) => {
 				if (error.message === 'Timeout' || error.message === 'Network request failed') {
 					me.setState({ showAlert: true, textoAlert: "Problemas de conexión" });
 				}
 			})
 		}
 	}
-
 	render() {
-
-		const { textoAlert, showAlert, name, email, birth_date, gender,  phone } = this.state;
-
+		const { textoAlert, showAlert, name, email, birth_date, gender, phone } = this.state;
 		return (
 			<React.Fragment>
-
 				<Alerta showAlert={showAlert} textoAlert={textoAlert} close={() => this.setState({ showAlert: false })} />
-
 				<div className="">
-
 					<div className="info_perfil_comp">
 						<h1 className="titleRegister">Tus datos</h1>
-
-						<form className="w3-container">
-
-                            <div>
-                                <label>Nombre y apellidos*</label>
-							    <input className="w3-input w3-border w3-round-large" type="text" value={name}
-								onChange={(e) => this.setState({ name: e.target.value })} />
-                            </div>
-
-                            <div>
-                                <label>Correo*</label>
-							    <input className="w3-input w3-border w3-round-large" type="text" value={email}
-								onChange={(e) => this.setState({ email: e.target.value })} />
-                            </div>
-							
-
+						<div className="w3-container">
 							<div>
-                                <label>Fecha de nacimiento*</label>
-							    <input className="w3-input w3-border w3-round-large size200" type="date" value={birth_date}
-								max={fechaAutorizada()}
-								onChange={(e) => this.setState({ birth_date: e.target.value })} />
-                            </div>
-
-                            <div>
-                                <label>Género*</label>
-                                <div>
-                                    <select className="w3-select w3-border w3-round-large w3-margin-bottom size200" name="gender"
-                                        value={gender} onChange={(e) => this.setState({ gender: e.target.value })}>
-                                        {this.gender_type.map((gender_type, key) => (
-                                            <option key={key} value={gender_type.id} >{gender_type.denomination}</option>
-                                        ))}
-                                    </select>
-                                </div>
-                            </div>
-
-                            <div>
-                                <label>Teléfono*</label>
-							    <input className="w3-input w3-border w3-round-large" type="number" value={phone}
-								onChange={(e) => this.setState({ phone: e.target.value })} />
-                            </div>
-
-							<p><button className="w3-button btn"
-								onClick={(e) => {
-									e.preventDefault();
-									this.guardar();
-								}}>Guardar</button></p>
-
-						</form>
+								<label>Nombre y apellidos*</label>
+								<input className="w3-input w3-border w3-round-large" type="text" value={name}
+									onChange={(e) => this.setState({ name: e.target.value })} />
+							</div>
+							<div>
+								<label>Correo*</label>
+								<input className="w3-input w3-border w3-round-large" type="text" value={email}
+									onChange={(e) => this.setState({ email: e.target.value })} />
+							</div>
+							<div>
+								<label>Fecha de nacimiento*</label>
+								<input className="w3-input w3-border w3-round-large size200" type="date" value={birth_date}
+									max={fechaAutorizada()}
+									onChange={(e) => this.setState({ birth_date: e.target.value })} />
+							</div>
+							<div>
+								<label>Género*</label>
+								<div>
+									<select className="w3-select w3-border w3-round-large w3-margin-bottom size200" name="gender"
+										value={gender} onChange={(e) => this.setState({ gender: e.target.value })}>
+										{this.gender_type.map((gender_type, key) => (
+											<option key={key} value={gender_type.id} >{gender_type.denomination}</option>
+										))}
+									</select>
+								</div>
+							</div>
+							<div>
+								<label>Teléfono*</label>
+								<input className="w3-input w3-border w3-round-large" type="number" value={phone}
+									onChange={(e) => this.setState({ phone: e.target.value })} />
+							</div>
+							<p><button className="w3-button btn" onClick={() => { this.guardar(); }}>Guardar</button></p>
+						</div>
 					</div>
 				</div>
 			</React.Fragment >
 		);
 	}
 }
-
-export default Registro;
+export default PerfilInformacion;
--- src/vistas/perfil/perfilVista.js
+++ src/vistas/perfil/perfilVista.js
@@ -9,82 +9,65 @@ import PerfilPolitica from "./perfilPolitica"
 import CargarImagenes from "../../componentes/cargarImagenes";
 import { Link } from "react-router-dom";
 import { Route, Switch } from "react-router";
-
-
+import httpClient from "../../constantes/axios";
 class Perfil extends React.Component {
 	constructor(props) {
-		super(props); 
-		this.state = { 
-			textoAlert	: "", 
-			showAlert	: false, 
-			photo		: "https://api.fixperto.com/uploads/registros/cliente/" +  JSON.parse(localStorage.getItem("@USER"))["avatar"],
-			cliente		: JSON.parse(localStorage.getItem("@USER")),
-			rutas 		: {
-				info 		: true,
-				config   	: false,
-				contraseña  : false,
-				quienes 	: false,
-				atencion 	: false,
-				terminos 	: false,
-				politica 	: false,
+		super(props);
+		this.state = {
+			textoAlert: "",
+			showAlert: false,
+			photo: httpClient.urlBase + "/uploads/registros/cliente/" + JSON.parse(localStorage.getItem("@USER"))["avatar"],
+			cliente: JSON.parse(localStorage.getItem("@USER")),
+			rutas: {
+				info: true, config: false, contraseña: false, quienes: false, atencion: false, terminos: false, politica: false,
 			}
-			
-
 		};
 	}
-
-	active = (text) => { 
-		let rutas = this.state['rutas'];
-		Object.keys(rutas).map((key) => { rutas[key] = (key === text)}); 
-		this.setState({rutas}); 
+	active = (text) => {
+		let rutas = this.state['rutas']; Object.keys(rutas).map((key) => { rutas[key] = (key === text) }); this.setState({ rutas });
 	}
-
-
 	componentDidMount() {
 		let rutas = {};
 		switch (this.props["history"]["location"]["pathname"]) {
 			case "/fixperto/perfil/perfil-informacion":
-				rutas = { info: true, config: false, contraseña: false, quienes : false, atencion : false, terminos : false, politica : false }
-				this.setState({rutas});
+				rutas = { info: true, config: false, contraseña: false, quienes: false, atencion: false, terminos: false, politica: false }
+				this.setState({ rutas });
 				break;
 			case "/fixperto/perfil/configuracion":
-				rutas = { info: false, config: true, contraseña: false, quienes : false, atencion : false, terminos : false, politica : false }
-				this.setState({rutas});
+				rutas = { info: false, config: true, contraseña: false, quienes: false, atencion: false, terminos: false, politica: false }
+				this.setState({ rutas });
 				break;
 			case "/fixperto/perfil/cambio_password":
-				rutas = { info: false, config: false, contraseña: true, quienes : false, atencion : false, terminos : false, politica : false }
-				this.setState({rutas});
+				rutas = { info: false, config: false, contraseña: true, quienes: false, atencion: false, terminos: false, politica: false }
+				this.setState({ rutas });
 				break;
 			case "/fixperto/perfil/quienes_somos":
-				rutas = { info: false, config: false, contraseña: false, quienes : true, atencion : false, terminos : false, politica : false }
-				this.setState({rutas});
+				rutas = { info: false, config: false, contraseña: false, quienes: true, atencion: false, terminos: false, politica: false }
+				this.setState({ rutas });
 				break;
 			case "/fixperto/perfil/atencion_cliente":
-				rutas = { info: false, config: false, contraseña: false, atencion : true, terminos : false, politica : false }
-				this.setState({rutas});
+				rutas = { info: false, config: false, contraseña: false, atencion: true, terminos: false, politica: false }
+				this.setState({ rutas });
 				break;
 			case "/fixperto/perfil/terminos_condiciones":
-				rutas = { info: false, config: false, contraseña: false, atencion : false, terminos : true, politica : false }
-				this.setState({rutas});
+				rutas = { info: false, config: false, contraseña: false, atencion: false, terminos: true, politica: false }
+				this.setState({ rutas });
 				break;
 			case "/fixperto/perfil/politica_privacidad":
-				rutas = { info: false, config: false, contraseña: false, atencion : false, terminos : false, politica : true }
-				this.setState({rutas});
+				rutas = { info: false, config: false, contraseña: false, atencion: false, terminos: false, politica: true }
+				this.setState({ rutas });
 				break;
 			default:
 				break;
 		}
 	}
-
+	actualizar = () => { this.setState({ cliente: JSON.parse(localStorage.getItem("@USER")) }); }
 	render() {
-
-		const { info, config, contraseña, quienes, atencion, terminos, politica  } = this.state;
+		const { info, config, contraseña, quienes, atencion, terminos, politica } = this.state;
 		const itemStyle = "active";
-		
 		return (
 			<React.Fragment>
 				<div className="container">
-
 					<div className="perfil">
 						<div className="w3-cell-row w3-margin-bottom">
 							<div className="w3-cell" style={{ width: 35 + "px" }}>
@@ -93,40 +76,34 @@ class Perfil extends React.Component {
 							<h2 className="w3-cell text_blue" >Perfil</h2>
 						</div>
 						<div className="w3-row">
-
 							<div className="w3-col s12 m5">
 								<div className="w3-card card_perfil">
 									<div className="w3-row info_person">
 										<div className="w3-col s12 l5">
-											{ this.state['photo'] === "" ? 
+											{this.state['photo'] === "" ?
 												<div className="w3-center img_upl">
-													<img src={this.state['photo']}  style={{width : 100, height:100}}/>
+													<img src={this.state['photo']} style={{ width: 100, height: 100 }} />
 												</div>
 												: <form>
 													<div>
-														<img src={this.state['photo']}  style={{width : 100, height:100}}/>
+														<img src={this.state['photo']} style={{ width: 100, height: 100 }} />
 
-														<CargarImagenes mod={ (photo, name) => { 
-															this.setState({ photo }); 
-																var user = JSON.parse(localStorage.getItem("@USER"))
-																user['avatar'] = name;
-																localStorage.setItem("@USER", JSON.stringify(user))
-															} 
+														<CargarImagenes mod={(photo, name) => {
+															this.setState({ photo });
+															var user = JSON.parse(localStorage.getItem("@USER"))
+															user['avatar'] = name;
+															localStorage.setItem("@USER", JSON.stringify(user))
+														}
 														} />
 													</div>
 												</form>
 											}
-											
 										</div>
 										<div className="w3-col s10 l7">
 											<h3 className="text_blue">¡Hola!</h3>
 											<h6>{this.state['cliente']['name']}</h6>
 										</div>
-										<div className="w3-row">
-
-										</div>
 									</div>
-
 									<div className="w3-row copy">
 										<div className="w3-col s3">
 											<img src="../../assets/iconos/alert.png" className=" img_alert" alt="alert"></img>
@@ -135,16 +112,13 @@ class Perfil extends React.Component {
 											<p>Modifica los datos de tu perfil</p>
 										</div>
 									</div>
-
 									<div>
 										<div className="w3-row title_datos">
 											<h2>
 												<img src="../../assets/iconos/solicit.png" className=" img_tus_datos" alt="solicitud"></img>
 											Tus datos</h2>
 										</div>
-
 										<div className="w3-row list_datos">
-
 											<div className="w3-row list">
 												<div className="w3-col s10">
 													<Link to="/fixperto/perfil/perfil-informacion" className={(info) ? `${itemStyle}` : ``} onClick={() => this.active("info")}>Información personal</Link>
@@ -155,7 +129,6 @@ class Perfil extends React.Component {
 													</Link>
 												</div>
 											</div>
-
 											<div className="w3-row list">
 												<div className="w3-col s10">
 													<Link to="/fixperto/perfil/configuracion" className={(config) ? `${itemStyle}` : ``} onClick={() => this.active("config")}>Configuración</Link>
@@ -164,10 +137,8 @@ class Perfil extends React.Component {
 													<Link to="/fixperto/perfil/configuracion" className="">
 														<img src="../../assets/iconos/continuar.png" className=" img_continuar" alt="continuar"></img>
 													</Link>
-
 												</div>
 											</div>
-
 											<div className="w3-row list">
 												<div className="w3-col s10">
 													<Link to="/fixperto/perfil/cambio_password" className={(contraseña) ? `${itemStyle}` : ``} onClick={() => this.active("contraseña")}>Cambiar contraseña</Link>
@@ -176,22 +147,17 @@ class Perfil extends React.Component {
 													<Link to="/fixperto/perfil/cambio_password" className="">
 														<img src="../../assets/iconos/continuar.png" className=" img_continuar" alt="continuar"></img>
 													</Link>
-
 												</div>
 											</div>
-
 										</div>
 									</div>
-
 									<div>
 										<div className="w3-row title_datos">
 											<h2>
 												<img src="../../assets/iconos/solicit.png" className=" img_tus_datos" alt="solicitud"></img>
 											Sobre Fixperto</h2>
 										</div>
-
 										<div className="w3-row list_datos">
-
 											<div className="w3-row list">
 												<div className="w3-col s10">
 													<Link to="/fixperto/perfil/quienes_somos" className={(quienes) ? `${itemStyle}` : ``} onClick={() => this.active("quienes")}>Quienes somos</Link>
@@ -200,10 +166,8 @@ class Perfil extends React.Component {
 													<Link to="/fixperto/perfil/quienes_somos" className="">
 														<img src="../../assets/iconos/continuar.png" className=" img_continuar" alt="continuar"></img>
 													</Link>
-
 												</div>
 											</div>
-
 											<div className="w3-row list">
 												<div className="w3-col s10">
 													<Link to="/fixperto/perfil/atencion_cliente" className={(atencion) ? `${itemStyle}` : ``} onClick={() => this.active("atencion")}>Atención al cliente</Link>
@@ -212,10 +176,8 @@ class Perfil extends React.Component {
 													<Link to="/fixperto/perfil/atencion_cliente" className="">
 														<img src="../../assets/iconos/continuar.png" className=" img_continuar" alt="continuar"></img>
 													</Link>
-
 												</div>
 											</div>
-
 											<div className="w3-row list">
 												<div className="w3-col s10">
 													<Link to="/fixperto/perfil/terminos_condiciones" className={(terminos) ? `${itemStyle}` : ``} onClick={() => this.active("terminos")}>Términos y condiciones</Link>
@@ -224,10 +186,8 @@ class Perfil extends React.Component {
 													<Link to="/fixperto/perfil/terminos_condiciones" className="">
 														<img src="../../assets/iconos/continuar.png" className=" img_continuar" alt="continuar"></img>
 													</Link>
-
 												</div>
 											</div>
-
 											<div className="w3-row list">
 												<div className="w3-col s10">
 													<Link to="/fixperto/perfil/politica_privacidad" className={(politica) ? `${itemStyle}` : ``} onClick={() => this.active("politica")}>Política y privacidad</Link>
@@ -236,30 +196,24 @@ class Perfil extends React.Component {
 													<Link to="/fixperto/perfil/politica_privacidad" className="">
 														<img src="../../assets/iconos/continuar.png" className=" img_continuar" alt="continuar"></img>
 													</Link>
-
 												</div>
 											</div>
 										</div>
 									</div>
-
 									<div className="w3-row btn_close">
 										<button className="w3-button "
-										onClick={() => { 
-											localStorage.setItem("@USER", JSON.stringify({}))
-											this.props.history.push({
-												pathname: '/ingreso',
-											})
-										}}> CERRAR SESIÓN </button>
+											onClick={() => {
+												localStorage.setItem("@USER", JSON.stringify({}));
+												this.props.history.push({ pathname: '/ingreso', });
+											}}> CERRAR SESIÓN</button>
 									</div>
-
 								</div>
-
 							</div>
-
 							{this.props["history"]["location"]["pathname"] !== "/fixperto/perfil/" && <div className="w3-col s12 m7">
 								<div className="w3-card card_info">
 									<Switch>
-										<Route path="/fixperto/perfil/perfil-informacion" component={PerfilInformacion} />
+										{/*	<Route path="/fixperto/perfil/perfil-informacion" component={PerfilInformacion} />*/}
+										<Route path="/fixperto/perfil/perfil-informacion" render={() => <PerfilInformacion actualizar={() => { this.actualizar() }} />} />
 										<Route path="/fixperto/perfil/configuracion" component={PerfilConfig} />
 										<Route path="/fixperto/perfil/cambio_password" component={PerfilCambiarContr} />
 										<Route path="/fixperto/perfil/quienes_somos" component={PerfilQuienesSomos} />
