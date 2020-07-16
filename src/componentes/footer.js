061d3ee6b3b623c43b0c22efdacace812b5e7f9f
alainshiftactive <alain.chacon@shiftactive.com>
2020-07-16 15:16:51 -0500

    r en fixperto del footer

--- src/componentes/footer.js
+++ src/componentes/footer.js
@@ -9,7 +9,7 @@ const Footer = (props) => {
 				<div className="w3-half w3-right w3-section w3-text-white apps" >
 					<div className="w3-row">
 						<div style={{ marginBottom: 10 + "px" }}>
-							<b>Descarga fixperto</b>
+							<b>Descarga fixperto®</b>
 						</div>
 						<div className="w3-cell">
 							<a href="https://play.google.com/store/apps/details?id=com.shiftactive.fixperto&hl=es_CO" target="_blank">
