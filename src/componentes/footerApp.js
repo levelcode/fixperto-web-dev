58aa1d2a83e80f97593882578e0130076b5dc277
alainshiftactive <alain.chacon@shiftactive.com>
2020-07-16 15:15:18 -0500

    imagenes del footer

--- src/componentes/footerApp.js
+++ src/componentes/footerApp.js
@@ -9,17 +9,17 @@ const FooterApp = (props) => {
 				<div className="w3-half w3-right w3-section w3-text-white apps">
 					<div className="w3-row">
 						<div style={{ marginBottom: 10 + "px" }}>
-							<b>Descarga fixperto</b>
+							<b>Descarga fixperto®</b>
 						</div>
 						<div className="w3-cell">
 							<a href="https://play.google.com/store/apps/details?id=com.shiftactive.fixperto&hl=es_CO" target="_blank">
-								<img src="./assets/google-play.png" className="footerFixperto" alt="Norway" />
+								<img src="../../assets/google-play.png" className="footerFixperto" alt="Norway" />
 							</a>
 							
 						</div>
 						<div className="w3-cell w3-container">
 							<a href="https://www.apple.com/co/ios/app-store/" target="_blank">
-								<img src="./assets/app-store.png" className="footerFixperto" alt="Norway" />
+								<img src="../../assets/app-store.png" className="footerFixperto" alt="Norway" />
 							</a>
 						</div>
 						<p style={{ fontSize: 10 + "px" }}>2020 fixperto Todos los derechos reservados</p>
