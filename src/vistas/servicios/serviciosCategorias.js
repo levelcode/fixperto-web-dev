e25cfdcd0ba2eb3d9a49e4bf3365a9851996b932
alainshiftactive <alain.chacon@shiftactive.com>
2020-07-24 10:09:16 -0500

    prueba

--- src/vistas/servicios/serviciosCategorias.js
+++ src/vistas/servicios/serviciosCategorias.js
@@ -17,13 +17,13 @@ class ServiciosCateg extends React.Component {
 				parsed["service"]=JSON.parse(parsed["service"]);
 				localStorage.setItem("@SEARCHCAT", JSON.stringify(parsed["service"]));
 			}
-			this.props["history"]["push"]("ingreso");
+			this.props["history"]["go"]("ingreso");
 		}
 		else {
 			if (user["type"] !== "cliente") {
 				localStorage.setItem("@SEARCHCAT", JSON.stringify({}));
 				localStorage.setItem("@USER", JSON.stringify({}));
-				this.props["history"]["push"]("ingreso");
+				this.props["history"]["go"]("ingreso");
 			}
 			else {
 				if (this.props["location"] && this.props["location"]["search"] !== "") {
